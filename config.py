62b2738482fe718c33ec0c0a684abd4b24586c2b
jkrumm <jokrumm@gmail.com>
2020-12-20 12:32:02 +0100

    New Algo for trades and curreny mappings

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, "algorithmic-trading", ".env"))
 
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,4 +12,5 @@ DEBUG = True
 
 # Secret key for session management. You can generate random strings here:
 # https://randomkeygen.com/
-SECRET_KEY = 'my precious'
+SECRET_KEY = os.getenv("SECRET_KEY")
+NOMICS_KEY = os.getenv("NOMICS_KEY")
